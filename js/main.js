1dcf70046f766b7a681fd41a617fdaade0e052c0
abeer26omar <abeer.omar504@gmail.com>
2021-10-07 23:05:18 +0200

    added gallery banner for images and offers

--- js/main.js
+++ js/main.js
@@ -12,9 +12,45 @@ var menu = document.getElementById("menu-ul");
 btn.addEventListener('click', function(){
   if(menu.style.display === "none"){
     menu.style.display = "flex";
+    btn.classList.add('open');
   }
   else
   {
-    menu.style.display = "none"; 
+    menu.style.display = "none";
+    btn.classList.remove('open'); 
   }
 })
+// MouseHover Animation 
+ var hoverLayer2 = $(".burger-promo-area");
+ var heroImgfive = $(".sh-1");
+ var heroImgsix = $(".sh-2");
+ var heroImgseven = $(".sh-5");
+ var heroImgeight = $(".sh-6");
+ hoverLayer2.mousemove(function (e) {
+   var valueX = (e.pageX * -1) / 100;
+   var valueY = (e.pageY * -1) / 120;
+   heroImgfive.css({
+     transform: "translate3d(" + valueX + "px," + valueY + "px, 0)"
+   });
+ });
+ hoverLayer2.mousemove(function (e) {
+   var valueX = (e.pageX * -1) / 60;
+   var valueY = (e.pageY * -1) / 80;
+   heroImgsix.css({
+     transform: "translate3d(" + valueX + "px," + valueY + "px, 0)"
+   });
+ });
+ hoverLayer2.mousemove(function (e) {
+   var valueX = (e.pageX * -1) / 40;
+   var valueY = (e.pageY * -1) / 60;
+   heroImgseven.css({
+     transform: "translate3d(" + valueX + "px," + valueY + "px, 0)"
+   });
+ });
+ hoverLayer2.mousemove(function (e) {
+   var valueX = (e.pageX * -1) / 80;
+   var valueY = (e.pageY * -1) / 100;
+   heroImgeight.css({
+     transform: "translate3d(" + valueX + "px," + valueY + "px, 0)"
+   });
+ });
