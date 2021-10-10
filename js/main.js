e006ad5302186cfd9ce1e3259307965c43380bc3
abeer26omar <abeer.omar504@gmail.com>
2021-10-10 19:34:09 +0200

    added menu section

--- js/main.js
+++ js/main.js
@@ -54,3 +54,31 @@ btn.addEventListener('click', function(){
      transform: "translate3d(" + valueX + "px," + valueY + "px, 0)"
    });
  });
+//menu navigator
+// $('#nav-tab a').on('click', function (e) {
+//   e.preventDefault()
+//   $(this).tab('show');
+//   document.getElementsByClassName('tap-pane') = "block";
+// })
+
+// var tabEl = document.querySelector('button[data-bs-toggle="tab"]')
+// tabEl.addEventListener('shown.bs.tab', function (event) {
+//   event.target // newly activated tab
+//   event.relatedTarget // previous active tab
+// })
+// var triggerTabList = [].slice.call(document.querySelectorAll('#nav-menu-1'))
+// triggerTabList.forEach(function (triggerEl) {
+//   var tabTrigger = new bootstrap.Tab(triggerEl)
+
+//   triggerEl.addEventListener('click', function (event) {
+//     event.preventDefault()
+//     tabTrigger.show()
+//   })
+// });
+ 
+// $('#nav-tab a').on('click',function(event){
+//  event.preventDefault();
+//  if($('#nav-tab a').hasAttribute('class','active')){
+//   console.log('fuck off')
+// }
+// })
